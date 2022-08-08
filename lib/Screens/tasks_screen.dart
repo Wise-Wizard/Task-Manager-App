f6c5e03707774f9bb557d2d1834661f033cce9fc
Wise-Wizard <saransh.shankar@gmail.com>
2022-08-08 16:45:57 +0530

    UI

---
+++ lib/Screens/tasks_screen.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class TasksScreens extends StatelessWidget {
+  const TasksScreens({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF3330E4),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        onPressed: () {},
+        backgroundColor: Color(0xFF3330E4),
+        child: Icon(
+          Icons.add,
+          size: 40,
+          color: Color(0xFFFAEA48),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 60, horizontal: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Color(0xFFFAEA48),
+                  child: Icon(
+                    Icons.access_alarm_sharp,
+                    color: Color(0xFF3330E4),
+                    size: 40,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Task Manager',
+                  style: TextStyle(color: Color(0xFFFAEA48), fontSize: 54),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(fontSize: 25, color: Color(0xFFFAEA48)),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFF637EC),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+              child: ListView(
+                children: [ListTile()],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
