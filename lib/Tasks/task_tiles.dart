bd637c54157b7b0fbc544629dbe5b266500f5608
Wise-Wizard <saransh.shankar@gmail.com>
2022-08-09 13:04:23 +0530

    Checkbox

--- lib/Tasks/task_tiles.dart
+++ lib/Tasks/task_tiles.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -8,12 +8,34 @@ class TasksTile extends StatelessWidget {
     return ListTile(
       title: const Text(
         'Task1',
-        style: TextStyle(color: Color(0xFFFAEA48), fontSize: 69),
+        style: TextStyle(
+            color: Color(0xFFFAEA48),
+            fontSize: 45,
+            fontWeight: FontWeight.w900),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  var isChecked;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Color(0xFF3330E4),
+      value: isChecked,
+      onChanged: (value) {
+        setState(() {
+          isChecked = value;
+        });
+      },
     );
   }
 }
